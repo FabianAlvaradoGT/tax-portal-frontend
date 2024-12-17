8a71d12f5513ba1de52896d27c379dabcdc9f8fd
FabianAlvaradoGT <fabian.alvarado@cl.gt.com>
2024-12-17 16:28:19 -0300

    Update page title and BlankView title to include home icon

--- src/pages/dashboard/one.tsx
+++ src/pages/dashboard/one.tsx
@@ -6,7 +6,7 @@ import { BlankView } from 'src/sections/blank/view'
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Page one | Dashboard - ${CONFIG.appName}` }
+const metadata = { title: `Home 🏠 - ${CONFIG.appName}` }
 
 export default function Page() {
   return (
@@ -15,7 +15,7 @@ export default function Page() {
         <title> {metadata.title}</title>
       </Helmet>
 
-      <BlankView title="Home" />
+      <BlankView title="Home 🏠" />
     </>
   )
 }
